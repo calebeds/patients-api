99c29f31ac67aa23b031250aa76cf9121320ae23
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-10-30 23:29:53 -0300

    fixup routes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import Response from "./domain/response.js";
 //import logger from './util/logger.js';
 import HttpStatus from "./controller/patient.controller.js";
+import patientRoutes from "./route/patient.route.js";
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+app.use("/patients", patientRoutes);
 app.get("/", (req, res) => {
   res.send(
     new Response(
@@ -20,6 +22,17 @@ app.get("/", (req, res) => {
       "Patient API, V1.0.0 - All Systems GO"
     )
   );
+});
+app.all("*", (req, res) => {
+  res
+    .status(HttpStatus.NOT_FOUND.code)
+    .send(
+      new Response(
+        HttpStatus.NOT_FOUND.code,
+        HttpStatus.NOT_FOUND.status,
+        "Route does not exist on the server"
+      )
+    );
 });
 app.listen(PORT, () =>
   console.log(`Server running on: ${ip.address()}:${PORT}`)
--- src/route/patient.route.js
+++ src/route/patient.route.js
@@ -9,10 +9,12 @@ import {
 
 const patientRoutes = express.Router();
 
-patientRoutes.get("/").get(getPatients).post(createPatient);
+patientRoutes.route("/").get(getPatients).post(createPatient);
 
 patientRoutes
   .route("/:id")
   .get(getPatient)
   .put(updatePatient)
   .delete(deletePatient);
+
+export default patientRoutes;
